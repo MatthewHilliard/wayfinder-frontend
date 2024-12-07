57efb499313c63470d50a679c5e63afbc71b140f
MatthewHilliard <matthewhilliard2003@gmail.com>
2024-12-06 22:51:00 -0500

    Attempting prod image fix

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,19 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
+      // Development environment
       {
         protocol: "http",
         hostname: "localhost",
         port: "8000",
         pathname: "/**",
+      },
+      // Production environment
+      {
+        protocol: "https",
+        hostname: "wayfinder-backend-prod-b2b08ed79f38.herokuapp.com",
+        port: "",
+        pathname: "/media/**",
       },
     ],
   },
